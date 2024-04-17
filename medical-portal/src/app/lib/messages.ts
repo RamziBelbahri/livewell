629677650a8bcb098db9b86f075bd8c5cd450373
Ramzi <ramzibelbahri@gmail.com>
2024-04-17 17:36:47 -0400

    added chat route

---
+++ medical-portal/src/app/lib/messages.ts
@@ -0,0 +1,5 @@
+export type message = {
+    userId: number;
+    content: string;
+    time: Date;
+}
---
+++ medical-portal/src/app/lib/placeholder-data.js
@@ -0,0 +1,18 @@
+const messages = [
+    {
+        userId: 1,
+        content: 'hello world',
+        time: new Date()
+    },
+    {
+        userId: 2,
+        content: 'hello to you',
+        time: new Date()
+    },
+    {
+        userId: 3,
+        content: 'hello as well',
+        time: new Date()
+    },
+
+];
